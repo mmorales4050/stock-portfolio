6c12201a44d6205677bf50d6fe86ec5d93122216
mmorales4050 <mmorales4050@gmail.com>
2019-09-29 14:52:29 -0400

    update login form

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,11 +4,27 @@ import 'semantic-ui-css/semantic.min.css'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  state = {
+    signIn: true
+  }
+
+  toggleSignIn = (e) => {
+    e.preventDefault()
+    this.setState({singIn: !this.state.singIn})
+  }
+
+  renderLogin = () => {
+    if(this.state.signIn) {
+      return <LoginForm singIn={true} toggleSignIn={this.toggleSignIn}/>
+    }else {
+      return <LoginForm singIn={false} toggleSignIn={this.toggleSignIn}/>
+    }
+  }
 
   render() {
     return (
       <div className="App">
-      <LoginForm />
+      {this.renderLogin()}
       </div>
     );
   }
--- ui/src/components/LoginForm.js
+++ ui/src/components/LoginForm.js
@@ -1,32 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 
-const LoginForm = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        Sign In
-      </Header>
-      <Form size='large'>
-        <Segment >
-          <Form.Input fluid  placeholder='email' />
-          <Form.Input
-            fluid
-            placeholder='password'
-            type='password'
-          />
+class LoginForm extends Component {
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        <a href='#'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+
+
+
+  render() {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'>
+            {this.props.signIn ? "Sign In" : "Register"}
+          </Header>
+          <Form size='large'>
+            <Segment>
+              {this.props.signIn ? null : <Form.Input fluid  placeholder='name'/>}
+              <Form.Input fluid  placeholder='email'/>
+              <Form.Input fluid placeholder='password'/>
+              <Button color='teal' fluid size='large'>
+                {this.props.signIn ? "Login" : "Create Account"}
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            <a href='#' onClick={this.props.toggleSignIn}>
+            {this.props.signIn ? "Sign Up" : "Login"}
+            </a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
 
 export default LoginForm
