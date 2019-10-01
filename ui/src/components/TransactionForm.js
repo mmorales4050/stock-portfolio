922e133da7a0a3969fd1506360528e3b418e3e95
mmorales4050 <mmorales4050@gmail.com>
2019-10-01 15:06:44 -0400

    add handle change for transaction form

--- ui/src/components/TransactionForm.js
+++ ui/src/components/TransactionForm.js
@@ -3,15 +3,30 @@ import { Grid, Segment, Header, Input, Button } from 'semantic-ui-react'
 
 class TransactionForm extends Component {
 
+  state = {
+    ticker: "",
+    quantity: ""
+  }
+
+  handleChange = (e) => {
+    let newState = {...this.state}
+    newState[e.target.id] = e.target.value
+    this.setState(newState)
+  }
+
+  handleClick = (e) => {
+    //post call here
+  }
+
   render() {
     return (
       <Grid.Column>
       <Header as='h3' textAlign='center'>
       Cash - ${this.props.user.cash}
       </Header>
-        <Input placeholder='Ticker' />
+        <Input id='ticker' placeholder='Ticker' />
         <br/>
-        <Input placeholder='Qty' style={{padding: "10px"}}/>
+        <Input id='quantity' placeholder='Qty' style={{padding: "10px"}}/>
         <br/>
         <Button>Buy</Button>
       </Grid.Column>
