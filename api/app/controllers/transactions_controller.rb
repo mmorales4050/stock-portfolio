56eb5ddbc12fa58cdf128e3c3db95b9c7ccbe642
mmorales4050 <mmorales4050@gmail.com>
2019-10-02 15:32:44 -0400

    update stock api

--- api/app/controllers/transactions_controller.rb
+++ api/app/controllers/transactions_controller.rb
@@ -4,14 +4,14 @@ class TransactionsController < ApplicationController
     user = User.all.find_by email: params[:user][:email], password: params[:user][:password]
 
     # Check if user can afford stocks
-    if user.cash > params[:price]
+    if user.cash > (params[:price] * params[:shares])
       # Create Transaction
       transaction = Transaction.create ticker: params[:ticker], shares: params[:shares], price: params[:price], user_id: user.id
 
-      user.update cash: user.cash - params[:price]
+      user.update cash: user.cash - (params[:price] * params[:shares])
 
       # Update / Create new stock in portfolio
-      stock = Stock.all.find_by ticker: params[:ticker] user_id: user.id
+      stock = Stock.all.find_by ticker: params[:ticker], user_id: user.id
 
       if stock
         stock.update shares: stock.shares + params[:shares]
